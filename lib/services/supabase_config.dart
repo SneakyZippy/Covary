5aa3abce4f2f75ef37bf2eef67423fb77d572bbe
SneakyZippy <flex.zoeggeler@live.de>
2026-06-03 14:19:18 +0200

    Stop ignoring supabase_config.dart and commit keys for PWA deployment

---
+++ lib/services/supabase_config.dart
@@ -0,0 +1,12 @@
+class SupabaseConfig {
+  /// The URL of your Supabase project (found in Project Settings -> API)
+  static const String supabaseUrl = 'https://kemsanovebctmaalxmdu.supabase.co';
+
+  /// The public anon key of your Supabase project (found in Project Settings -> API)
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlbXNhbm92ZWJjdG1hYWx4bWR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMDE4MTcsImV4cCI6MjA5NDc3NzgxN30.aYQBqhcC28H5kXx3LxSDs4h6-lDeNsMp5npiH1Nd9Rk';
+
+  /// The public VAPID key used for web push subscription
+  static const String vapidPublicKey =
+      'BGfUusu_wlSew4213PsuwXUBSsoFoJXYFq2s0oTKT_X_yZ0Ai2w80iATkWjrNoMLuK9GVUrSJM3n2lDuA78G00I';
+}
